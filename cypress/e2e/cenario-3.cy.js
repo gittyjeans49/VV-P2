ee951057e42f96a4ad213c1775decdf027d1d537
davi.takayama <davi.takayama@kepha.com.br>
2023-06-23 11:31:53 -0300

    adição do teste no relatório

--- cypress/e2e/cenario-3.cy.js
+++ cypress/e2e/cenario-3.cy.js
@@ -1,10 +1,5 @@
 /// <reference types="cypress" />
 
-// Cenário 3: Adição de Produtos ao Carrinho
-// 1. Testar a funcionalidade de adicionar produtos ao carrinho.
-// 2. Verificar se os produtos são corretamente adicionados e refletidos no resumo do carrinho.
-// 3. Testar o limite máximo de produtos que podem ser adicionados ao carrinho e verificar se o sistema está tratando corretamente essa condição
-
 describe('Cenário 03 - Adição de Produtos ao Carrinho', () => {
     beforeEach(() => {
         cy.visit('https://www.saucedemo.com');
